75d84c7379e61301d8e7a8aa50bb5a5e2182918d
wesley <wesleyaraujoteixeira98@gmail.com>
2024-05-23 06:26:44 -0300

    finishing the last files

--- conversor-moedas/src/App.tsx
+++ conversor-moedas/src/App.tsx
@@ -1,42 +1,56 @@
 import { useState } from 'react';
 import './App.css';
+import { BsCashCoin } from "react-icons/bs";
+
 
 
 function App() {
 
-  const [quantia, setQuantia] = useState('');
+  const [quantia, setQuantia] = useState('1');
   const [deMoeda, setDeMoeda] = useState("BRL");
   const [paraMoeda, setParaMoeda] = useState("USD");
-  const [resultado, setResultado] = useState(0);
+  const [resultado, setResultado] = useState('');
 
 
   const converterMoeda = () => {
 
-    const host = "api.frankfurter.app";
+    if(quantia == '' || quantia == '0'){
+      alert(' Invalido')
+    }
 
+    const host = "api.frankfurter.app";
     fetch(`https://${host}/latest?amount=${quantia}&from=${deMoeda}&to=${paraMoeda}`)
     .then((resp) => resp.json())
-    .then((data) => setResultado(data.rates[paraMoeda]));
+    .then((data) => setResultado(String(data.rates[paraMoeda])));
+
+  }
+
+
+  const convertCoin = (money: number) => {
+    return money.toLocaleString('pt-BR', {
+      currency: 'BRL',
+      style: 'currency'
+    })
   }
 
 
   return (    
       <>
-        <div>
-
-            <div>
-              <label> Quantia: </label>
-              <input type="text" value={quantia} onChange={e => setQuantia(e.target.value)} />
+        <div className='main'>
+          <h1> Conversor de Moedas  <BsCashCoin /> </h1>
+            <div className='container-value'>
+              <label  className='label' htmlFor='value'> Quantia </label>
+              <input type="text" id='value'  value={quantia} onChange={e => setQuantia(e.target.value)} />
             </div>
 
-            <div>
+            <div className='select-content'>
               <select value={deMoeda} onChange={e => setDeMoeda(e.target.value)} >
                 <option value='USD' > DÓLAR </option>
                 <option value='BRL' > REAL </option>
                 <option value='EUR' > EURO </option>
                 <option value='GBP'> LIBRA </option>
               </select>
-              
+
               <select value={paraMoeda} onChange={e => setParaMoeda(e.target.value)} >
                 <option value='USD' > DÓLAR </option>
                 <option value='BRL' > REAL </option>
@@ -45,8 +59,8 @@ function App() {
               </select>
             </div>
 
-            <button onClick={() => converterMoeda()}> Converter </button>
-            <div> {resultado.toFixed(2)} </div>
+            <button className='button' onClick={() => converterMoeda()}> Converter </button>
+            <div className='result'> <h2> {convertCoin(Number(resultado))}</h2> </div>
         </div>     
       </>
   )
